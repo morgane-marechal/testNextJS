359dc0c4d1c0abc2935518d3b744bec3ed167c31
morgane-marechal <morgane.marechal@laplateforme.io>
2024-03-26 20:17:04 +0100

    ajout methodes avec prisma

--- src/app/page.js
+++ src/app/page.js
@@ -9,14 +9,19 @@ const prisma = new PrismaClient()
 // export default prisma;
 
 // run inside `async` function
-const newUser = await prisma.user.create({
-    data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
-    },
-  })
+// const newUser = await prisma.user.create({
+//     data: {
+//       name: 'Pepper',
+//       email: 'pepper@prisma.io',
+//     },
+//   })
   
   const users = await prisma.user.findMany()
+  const userOne= await prisma.user.findUnique({
+    where: {
+      id: 1,
+    },
+  })
 
 export default function Home() {
   return (
@@ -26,6 +31,9 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.js</code>
         </p>
+        
+{userOne.email + userOne.name}
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
