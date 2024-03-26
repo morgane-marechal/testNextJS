07df3dd6f48213e6b53ff5e46262f2b57ebf38b2
morgane-marechal <morgane.marechal@laplateforme.io>
2024-03-26 19:39:49 +0100

    mise en route de next

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
+// import Inter from 'next/font/google'; // Assurez-vous de fournir le chemin correct vers le fichier contenant la fonction Inter
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/* <body className={className}>{children}</body> */}
+      <body >{children}</body>
+
     </html>
   );
 }
+
+
