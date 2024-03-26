d708c678e88d1174f53239bcde22ca438cb88706
morgane-marechal <morgane.marechal@laplateforme.io>
2024-03-26 19:50:44 +0100

    ajout donnees avec prisma

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,22 @@
 const{Image} = require ("next/image");
 const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+// use `prisma` in your application to read and write data in your DB
+
+// const globalForPrisma = global as unknown as { prisma: PrismaClient; }; 
+// export const prisma = globalForPrisma.prisma || new PrismaClient(); 
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+// export default prisma;
+
+// run inside `async` function
+const newUser = await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+    },
+  })
+  
+  const users = await prisma.user.findMany()
 
 export default function Home() {
   return (
